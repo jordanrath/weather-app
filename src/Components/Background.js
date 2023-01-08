60d7bf86089e00a52532a6ecdfb014aea7fe4792
jordanrath <rath.jordan@icloud.com>
2023-01-08 16:30:41 -0700

    added comment in background.js

--- src/Components/Background.js
+++ src/Components/Background.js
@@ -5,8 +5,10 @@ import { getBackgroundFromWeatherType } from '../Data/BackgroundData';
 const Background = (props) => {
     const { weatherType, children } = props;
 
-    const { class: backgroundClass} = getBackgroundFromWeatherType(weatherType);
+    const { class: backgroundClass } = getBackgroundFromWeatherType(weatherType);
     const finalBackgroundClass = classNames(['background', backgroundClass])
+
+// destructure name and use popper to add title={name} to show the name on hover
 
   return (
     <div className={finalBackgroundClass}>
